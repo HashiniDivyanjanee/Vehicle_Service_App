810178f336ca92465f5d7e1934381a07e40b4572
Hashini Divyanjanee <143160465+HashiniDivyanjanee@users.noreply.github.com>
2024-11-11 16:14:10 +0530

    Job Details Page

--- lib/src/config/routes/app_rout_config.dart
+++ lib/src/config/routes/app_rout_config.dart
@@ -2,18 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:vehicle_service_app/src/config/routes/app_rout_cons.dart';
 import 'package:vehicle_service_app/src/ui/home.dart';
+import 'package:vehicle_service_app/src/ui/job_details.dart';
 import 'package:vehicle_service_app/src/ui/login.dart';
-
 
 final GoRouter router = GoRouter(
   routes: [
-      GoRoute(
-          name: MyAppRouteConstants.homeRoute,
-          path: '/',
-          pageBuilder: (context, state) {
-            return MaterialPage(child: Login());
-          },
-        ),
- 
+    GoRoute(
+      name: MyAppRouteConstants.loginRoute,
+      path: '/',
+      pageBuilder: (context, state) {
+        return MaterialPage(child: Login());
+      },
+    ),
+    GoRoute(
+      name: MyAppRouteConstants.homeRoute,
+      path: '/home',
+      pageBuilder: (context, state) {
+        return MaterialPage(child: Home());
+      },
+    ),
+     GoRoute(
+      name: MyAppRouteConstants.job_detailsRoute,
+      path: '/job_details',
+      pageBuilder: (context, state) {
+        return MaterialPage(child: JobDetails());
+      },
+    ),
   ],
 );
--- lib/src/config/routes/app_rout_cons.dart
+++ lib/src/config/routes/app_rout_cons.dart
@@ -1,3 +1,5 @@
-class MyAppRouteConstants{
+class MyAppRouteConstants {
   static const String homeRoute = 'home';
+  static const String loginRoute = 'login';
+  static const String job_detailsRoute = 'job_details';
 }
