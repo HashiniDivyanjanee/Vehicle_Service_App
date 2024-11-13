75528c188e28a43a2cf9567734fed7cb9efd369c
Hashini Divyanjanee <143160465+HashiniDivyanjanee@users.noreply.github.com>
2024-11-13 16:27:41 +0530

    Coded Select Date Field

---
+++ lib/src/core/widgets/select_date_field.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:vehicle_service_app/src/constant/themes.dart';
+
+class SelectDateField extends StatefulWidget {
+  SelectDateField({super.key});
+
+  @override
+  _SelectDateFieldState createState() => _SelectDateFieldState();
+}
+
+class _SelectDateFieldState extends State<SelectDateField> {
+  DateTime? selectedDate;
+
+  Future<void> selectDate(BuildContext context) async {
+    final DateTime? pickedDate = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2100),
+    );
+    if (pickedDate != null && pickedDate != selectedDate) {
+      setState(() {
+        selectedDate = pickedDate;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 15, left: 25, right: 25),
+      decoration: BoxDecoration(
+          color: AppThemes.TextField_Bg_Color,
+          borderRadius: BorderRadius.circular(30)),
+      child: TextField(
+        style: TextStyle(
+            fontSize: 15,
+            color: AppThemes.SecondTextColor,
+            fontWeight: FontWeight.normal),
+        readOnly: true,
+        decoration: InputDecoration(
+            hintText: selectedDate != null
+                ? "${selectedDate!.day}-${selectedDate!.month}-${selectedDate!.year}"
+                : "Schedule Date",
+            suffixIcon: Icon(
+              Icons.calendar_month,
+              color: AppThemes.PrimaryColor,
+            ),
+            border: OutlineInputBorder(borderSide: BorderSide.none)),
+        onTap: () => selectDate(context),
+      ),
+    );
+  }
+}
