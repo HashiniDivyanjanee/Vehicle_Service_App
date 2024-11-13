503d07f63b69e4dae6ae02ae496aa76dda2e3e1d
Hashini Divyanjanee <143160465+HashiniDivyanjanee@users.noreply.github.com>
2024-11-13 16:28:38 +0530

    Modify Sales Tab & Vehicle Info

--- lib/src/ui/tabs/service_type.dart
+++ lib/src/ui/tabs/service_type.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:vehicle_service_app/src/config/routes/app_rout_cons.dart';
 import 'package:vehicle_service_app/src/constant/themes.dart';
+import 'package:vehicle_service_app/src/core/widgets/buttons.dart';
 import 'package:vehicle_service_app/src/core/widgets/drop_down_menu_widget.dart';
+import 'package:vehicle_service_app/src/core/widgets/select_date_field.dart';
 import 'package:vehicle_service_app/src/core/widgets/text_form_field.dart';
 
 class ServiceType extends StatelessWidget {
@@ -12,75 +16,92 @@ class ServiceType extends StatelessWidget {
   // final nameController = TextEditingController();
   // final nameController = TextEditingController();
   // final nameController = TextEditingController();
-  // final nameController = TextEditingController(); 
+  // final nameController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Container(
         child: Padding(
       padding: const EdgeInsets.only(top: 30, left: 15, right: 15),
-      child: Column(
-        children: [
-          Text(
-            "SERVICE TYPE",
-            style: TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-            ),
+      child: Scrollbar(
+        thumbVisibility: true,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.topLeft,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 25),
+                  child: Text(
+                    "SERVICE TYPE",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 25),
+                  child: Text(
+                    "Enter Basic Details for new work order",
+                    style: TextStyle(
+                        color: AppThemes.SecondTextColor,
+                        fontSize: AppThemes.SecondaryFontSize),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              DropDownMenuWidget(),
+              SelectDateField(),
+              TextFormFieldComponent(
+                controller: nameController,
+                lableText: "Claim Availble",
+                suffixIcon: Icons.event_available,
+                inputType: TextInputType.name,
+                textCapitalization: TextCapitalization.words,
+                obscureText: false,
+                maxLines: 1,
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              TextFormFieldComponent(
+                controller: nameController,
+                lableText: "Chassis Number",
+                suffixIcon: Icons.numbers,
+                inputType: TextInputType.name,
+                textCapitalization: TextCapitalization.words,
+                obscureText: false,
+                maxLines: 1,
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              TextFormFieldComponent(
+                controller: nameController,
+                lableText: "Note",
+                suffixIcon: Icons.mode,
+                inputType: TextInputType.name,
+                textCapitalization: TextCapitalization.words,
+                obscureText: false,
+                maxLines: 5,
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              ButtonComponent(
+                  buttonText: "NEXT",
+                  textColor: Colors.white,
+                  buttonColor: AppThemes.PrimaryColor,
+                  callback: () {}),
+            ],
           ),
-          Text(
-            "Enter Basic Details for new work order",
-            style: TextStyle(
-                color: AppThemes.SecondTextColor,
-                fontSize: AppThemes.SecondaryFontSize),
-          ),
-          SizedBox(
-            height: 40,
-          ),
-          TextFormFieldComponent(
-            controller: nameController,
-            lableText: "Username",
-            prefixIcon: Icons.person,
-            inputType: TextInputType.name,
-            textCapitalization: TextCapitalization.words,
-            obscureText: false,
-          ),
-           SizedBox(
-            height: 5,
-          ),
-          TextFormFieldComponent(
-            controller: nameController,
-            lableText: "Username",
-            prefixIcon: Icons.person,
-            inputType: TextInputType.name,
-            textCapitalization: TextCapitalization.words,
-            obscureText: false,
-          ),
-           SizedBox(
-            height: 5,
-          ),
-          TextFormFieldComponent(
-            controller: nameController,
-            lableText: "Username",
-            prefixIcon: Icons.person,
-            inputType: TextInputType.name,
-            textCapitalization: TextCapitalization.words,
-            obscureText: false,
-          ),
-           SizedBox(
-            height: 5,
-          ),
-          TextFormFieldComponent(
-            controller: nameController,
-            lableText: "Username",
-            prefixIcon: Icons.person,
-            inputType: TextInputType.name,
-            textCapitalization: TextCapitalization.words,
-            obscureText: false,
-          ),
-           
-         DropDownMenuWidget() 
-        ],
+        ),
       ),
     ));
   }
--- lib/src/ui/tabs/vehical_information.dart
+++ lib/src/ui/tabs/vehical_information.dart
@@ -1,12 +1,130 @@
 import 'package:flutter/material.dart';
+import 'package:vehicle_service_app/src/constant/themes.dart';
+import 'package:vehicle_service_app/src/core/widgets/buttons.dart';
+import 'package:vehicle_service_app/src/core/widgets/text_form_field.dart';
 
 class VehicalInformation extends StatelessWidget {
-  const VehicalInformation({super.key});
-
+  VehicalInformation({super.key});
+  final nameController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(child: Text("data")),
-    );
+        child: Padding(
+      padding: const EdgeInsets.only(top: 30, left: 15, right: 15),
+      child: Scrollbar(
+         thumbVisibility: true, 
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.topLeft,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 25),
+                  child: Text(
+                    "VEHICLE INFORMATION",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.topLeft,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 25),
+                  child: Text(
+                    "Enter Vehicle Details",
+                    style: TextStyle(
+                        color: AppThemes.SecondTextColor,
+                        fontSize: AppThemes.SecondaryFontSize),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              TextFormFieldComponent(
+                controller: nameController,
+                lableText: "Vehicle Number",
+                suffixIcon: Icons.numbers,
+                inputType: TextInputType.name,
+                textCapitalization: TextCapitalization.words,
+                obscureText: false,
+                maxLines: 1,
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              TextFormFieldComponent(
+                controller: nameController,
+                lableText: "Current Mileage",
+                suffixIcon: Icons.height,
+                inputType: TextInputType.name,
+                textCapitalization: TextCapitalization.words,
+                obscureText: false,
+                maxLines: 1,
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              TextFormFieldComponent(
+                controller: nameController,
+                lableText: "Brand",
+                suffixIcon: Icons.badge_rounded,
+                inputType: TextInputType.name,
+                textCapitalization: TextCapitalization.words,
+                obscureText: false,
+                maxLines: 1,
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              TextFormFieldComponent(
+                controller: nameController,
+                lableText: "Model",
+                suffixIcon: Icons.model_training,
+                inputType: TextInputType.name,
+                textCapitalization: TextCapitalization.words,
+                obscureText: false,
+                maxLines: 1,
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              TextFormFieldComponent(
+                controller: nameController,
+                lableText: "Year",
+                suffixIcon: Icons.date_range,
+                inputType: TextInputType.name,
+                textCapitalization: TextCapitalization.words,
+                obscureText: false,
+                maxLines: 1,
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              TextFormFieldComponent(
+                controller: nameController,
+                lableText: "Color",
+                suffixIcon: Icons.color_lens,
+                inputType: TextInputType.name,
+                textCapitalization: TextCapitalization.words,
+                obscureText: false,
+                maxLines: 1,
+              ),
+               SizedBox(
+                height: 15,
+              ),
+               ButtonComponent(
+                  buttonText: "SAVE",
+                  textColor: Colors.white,
+                  buttonColor: AppThemes.PrimaryColor,
+                  callback: () {}),
+            ],
+          ),
+        ),
+      ),
+    ));
   }
 }
